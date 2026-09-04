219dff2c57172d37ab808c8890723522ce811dc8
Silvio David Rodriguez <silviodavidrodriguez@gmail.com>
2026-09-04 14:02:28 -0300

    feat: add SPC attribute chart engine

--- control/services/__init__.py
+++ control/services/__init__.py
@@ -9,6 +9,18 @@ from .constants import (
     XBAR_S_SUBGROUP_SIZES,
 )
 
+from .attributes import (
+    AttributeChartInputError,
+    CChartResult,
+    NPChartResult,
+    PChartResult,
+    UChartResult,
+    calculate_c_chart,
+    calculate_np_chart,
+    calculate_p_chart,
+    calculate_u_chart,
+)
+
 
 __all__ = [
     "INDIVIDUAL_MR_CONSTANTS",
@@ -19,4 +31,13 @@ __all__ = [
     "XBAR_R_SUBGROUP_SIZES",
     "XBAR_S_CONSTANTS",
     "XBAR_S_SUBGROUP_SIZES",
+    "AttributeChartInputError",
+    "CChartResult",
+    "NPChartResult",
+    "PChartResult",
+    "UChartResult",
+    "calculate_c_chart",
+    "calculate_np_chart",
+    "calculate_p_chart",
+    "calculate_u_chart",
 ]
